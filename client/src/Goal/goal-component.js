ec2ecc8d698bbd84fddfc6debc9483df4010b62c
Ryan Hill <ryanhill1128@gmail.com>
2020-08-05 21:52:10 -0500

    Renders a goal card with title and text seperated

--- client/src/Goal/goal-component.js
+++ client/src/Goal/goal-component.js
@@ -1,5 +1,13 @@
 import React from "react";
+import "./goal-component.css";
 
-const Goal = (props) => {};
+const Goal = (props) => {
+  return (
+    <div className="goal">
+      <h3 className="goal__title">{props.title}</h3>
+      <p className="goal__text">{props.text}</p>
+    </div>
+  );
+};
 
 export default Goal;
