22a2f8fbe407fec0261f0f924d087d901aabd6b0
Ryan Hill <ryanhill1128@gmail.com>
2020-08-06 13:44:07 -0500

    Clicking new goal button console logs a message

--- client/src/Navbar/navbar-component.js
+++ client/src/Navbar/navbar-component.js
@@ -1,7 +1,22 @@
 import React from "react";
 
+import "./navbar-component.css";
+
 const Navbar = (props) => {
-  return <div></div>;
+  const newGoalClickHandler = () => {
+    console.log("New Goal Button Clicked");
+  };
+
+  return (
+    <ul className="navbar">
+      <li className="navbar__li">
+        <h3>Goal Tracker</h3>
+      </li>
+      <li className="navbar__li" onClick={newGoalClickHandler}>
+        <p>New Goal</p>
+      </li>
+    </ul>
+  );
 };
 
 export default Navbar;
