2a648f9fbe145ff6f0726e2fa77fb46974ae3d36
Ryan Hill <ryanhill1128@gmail.com>
2020-08-07 13:31:16 -0500

    Added button clicked handler that is passed into navbar props

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import Navbar from "./Navbar/navbar-component";
 import Goal from "./Goal/goal-component";
+import NewGoal from "./NewGoal/newgoal-component";
 
 import "./App.css";
 
@@ -20,19 +21,30 @@ class App extends Component {
       .then((data) => this.setState({ goals: data.goals }));
   }
 
+  newGoalClickedHandler = () => {
+    const showing = this.state.newGoalShowing;
+    this.setState({ newGoalShowing: !showing });
+  };
+
   render() {
     if (this.state.newGoalShowing) {
       return (
         <div>
-          <Navbar />
-          <h2>New Goal Form</h2>
+          <Navbar
+            newGoalClicked={() => this.newGoalClickedHandler()}
+            buttonText="Exit"
+          />
+          <NewGoal />
         </div>
       );
     }
 
     return (
       <div>
-        <Navbar />
+        <Navbar
+          newGoalClicked={() => this.newGoalClickedHandler()}
+          buttonText="New Goal"
+        />
         <div>
           {this.state.goals.map((g) => (
             <Goal title={g.title} text={g.text} />
