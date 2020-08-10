38392c11220d61dc5dc9c8ad0646ef97217a02af
Ryan Hill <ryanhill1128@gmail.com>
2020-08-09 19:00:33 -0500

    Added delete button to goal cards; click handler logs goal title

--- client/src/Goal/goal-component.js
+++ client/src/Goal/goal-component.js
@@ -2,9 +2,18 @@ import React from "react";
 import "./goal-component.css";
 
 const Goal = (props) => {
+  const deleteGoalClickHandler = () => {
+    console.log(`Delete button clicked: ${props.title}`);
+  };
+
   return (
     <div className="goal">
-      <h3 className="goal__title">{props.title}</h3>
+      <div className="title-area">
+        <h3 className="goal__title">{props.title}</h3>
+        <div className="Edit">
+          <h5 onClick={() => deleteGoalClickHandler()}>Delete</h5>
+        </div>
+      </div>
       <p className="goal__text">{props.text}</p>
     </div>
   );
