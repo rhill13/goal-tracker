ead3c74552cd08ce78ab92faa18dfb9278651bda
Ryan Hill <ryanhill1128@gmail.com>
2020-08-14 23:47:18 -0500

    Added icons for edit and delete buttons, also edit mode but will remove

--- client/src/Goal/goal-component.js
+++ client/src/Goal/goal-component.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./goal-component.css";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
 const Goal = (props) => {
   const deleteGoalClickHandler = () => {
@@ -21,13 +24,27 @@ const Goal = (props) => {
     props.editGoalClicked();
   };
 
+  if (!props.editMode) {
+    return (
+      <div className="goal">
+        <div className="title-area">
+          <h3 className="goal__title">{props.title}</h3>
+        </div>
+        <p className="goal__text">{props.text}</p>
+      </div>
+    );
+  }
   return (
     <div className="goal">
       <div className="title-area">
         <h3 className="goal__title">{props.title}</h3>
         <div className="Edit">
-          <h5 onClick={() => editGoalClickHandler()}>Edit</h5>
-          <h5 onClick={() => deleteGoalClickHandler()}>Delete</h5>
+          <h5 onClick={() => editGoalClickHandler()}>
+            <FontAwesomeIcon icon={faEdit} />
+          </h5>
+          <h5 onClick={() => deleteGoalClickHandler()}>
+            <FontAwesomeIcon icon={faTrashAlt} />
+          </h5>
         </div>
       </div>
       <p className="goal__text">{props.text}</p>
