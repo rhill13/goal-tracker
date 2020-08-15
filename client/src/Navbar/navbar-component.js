19d4480603da210746857d7f287a16a41b2fc768
Ryan Hill <ryanhill1128@gmail.com>
2020-08-14 23:49:37 -0500

    Switched to class based component to help with edit mode toggle but will revert back due to removal of toggle

--- client/src/Navbar/navbar-component.js
+++ client/src/Navbar/navbar-component.js
@@ -1,22 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./navbar-component.css";
 
-const Navbar = (props) => {
-  const newGoalClickHandler = () => {
-    return props.newGoalClicked();
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeState: this.props.editActive,
+    };
+  }
+
+  newGoalClickHandler = () => {
+    const isActive = this.state.activeState;
+    this.setState({ activeState: !isActive }, () => {
+      return this.props.newGoalClicked();
+    });
   };
 
-  return (
-    <ul className="navbar">
-      <li className="navbar__li">
-        <h3>Goal Tracker</h3>
-      </li>
-      <li className="navbar__li" onClick={newGoalClickHandler}>
-        <p>{props.buttonText}</p>
-      </li>
-    </ul>
-  );
-};
+  editModeClickHandler = () => {
+    const isActive = this.state.activeState;
+    this.setState({ activeState: !isActive }, () => {
+      return this.props.editModeClickHandler();
+    });
+  };
+
+  render() {
+    return (
+      <div className="navbar-main">
+        <ul className="navbar-left">
+          <li>
+            <h3>Goal Tracker</h3>
+          </li>
+        </ul>
+        <ul className="navbar-right">
+          <li
+            className={this.state.activeState ? "active" : "navbar__li"}
+            onClick={this.editModeClickHandler}
+          >
+            <p>Edit Mode</p>
+          </li>
+          <li className="navbar__li" onClick={this.newGoalClickHandler}>
+            <p>{this.props.buttonText}</p>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Navbar;
